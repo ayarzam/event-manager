7131552b8f2e1363aa627b8aa4ee0b7406487811
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-12-20 19:53:32 -0500

    update server routes to inculde try catch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const db = require('./models/db');
 const app = require('./server/index');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const init = async () => {
  await db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
--- server/api/events.js
+++ server/api/events.js
@@ -1,39 +1,58 @@
 const router = require('express').Router();
+const morgan = require('morgan')
+const Sequelize = require('sequelize')
 const {Event} = require('../../models');
 
-router.get('/', (req, res, next) => {
-  Event.findAll()
-    .then(events => res.json(events))
-    .catch(next);
+router.get('/events', async(req, res, next) => {
+  try {
+    const eventData = await Event.findAll(req.params)
+    res.status(200).json(eventData)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get('/:eventId', (req, res, next) => {
-  Event.findById(req.params.eventId)
-    .then(event => res.json(event))
-    .catch(next);
+router.get('/events/:id', async(req, res, next) => {
+  try {
+    const singleEvent = await Event.findById(req.params.id)
+    res.json(singleEvent)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.post('/', (req, res, next) => {
-  Event.create(req.body)
-    .then(event => res.json(event))
-    .catch(next);
+router.post('/events', async(req, res, next) => {
+  try {
+    const newEvent = await Event.create(req.bod)
+    res.json(newEvent)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.put('/:eventId', (req, res, next) => {
-  Event.findById(req.params.eventId)
-    .then(event => event.update(req.body))
-    .then(event => res.json(event))
-    .catch(next);
+router.put('events/:id', async(req, res, next) => {
+  try {
+    const foundEvents = await Event.findById(req.params.id)
+    const newEvent = await foundEvents.update(req.body)
+    res.json(newEvent)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.delete('/:eventId', (req, res, next) => {
-  Event.destroy({
-    where: {
-      id: req.params.eventId
-    }
-  })
-    .then(() => res.status(204).end())
-    .catch(next);
+router.delete('events/:id', async(req, res, next) => {
+  try {
+    await Event.destroy(req.params.id)
+    res.status(204)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.use((req, res, next) => {
+  const err = new Error('API route not found!')
+  err.status = 404
+  next(err)
 })
 
 module.exports = router;
